4d2effe465950147aeddb42470080c163ffc0922
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-04-11 15:57:51 +0800

    Add demo

--- code-generator-basic/src/main/java/top/waterspo/generator/StaticGenerator.java
+++ code-generator-basic/src/main/java/top/waterspo/generator/StaticGenerator.java
@@ -1,4 +1,21 @@
 package top.waterspo.generator;
 
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
 public class StaticGenerator {
+    public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir");
+        String inputPath =projectPath+File.separator+ "yuzi-generator-demo-projects" + File.separator + "acm-template";
+        String outputPath = projectPath;
+        System.out.println(projectPath);
+        // copyFileByHutool(inputPath, outputPath);
+    }
+
+    public static void copyFileByHutool(String inputPath, String outputPath) {
+        // 使用Hutool工具类复制文件
+        FileUtil.copy(inputPath, outputPath, false);
+
+    }
 }
