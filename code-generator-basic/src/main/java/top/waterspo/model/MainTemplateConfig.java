318460eaadb68ffed2a14d5414c0750ff5c5a723
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-04-12 13:30:45 +0800

    feat: 添加动态代码生成功能
    新增动态代码生成器，使用FreeMarker模板引擎生成Java代码。添加MainTemplateConfig类用于配置模板参数，并实现DynamicGenerator类以根据配置生成代码文件。此功能提高了代码生成的灵活性和可维护性。

--- code-generator-basic/src/main/java/top/waterspo/model/MainTemplateConfig.java
+++ code-generator-basic/src/main/java/top/waterspo/model/MainTemplateConfig.java
@@ -1,4 +1,13 @@
 package top.waterspo.model;
 
+import lombok.Data;
+
+@Data
 public class MainTemplateConfig {
+
+    private String author;
+
+    private String outputText;
+
+    private boolean loop;
 }
